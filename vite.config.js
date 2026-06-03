8f04da6918e1e5722b0b54b6b9ce2805bffc9a16
ahmadfaraj4000-gif <ahmad.faraj4000@gmail.com>
2026-06-03 16:30:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/prostyle-admin/",
+
   appType: "spa",
+
   server: {
     port: 5174,
     strictPort: false
   },
+
   preview: {
     port: 4174,
     strictPort: false
